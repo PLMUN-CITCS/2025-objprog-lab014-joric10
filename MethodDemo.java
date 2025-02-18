a3dbf6b197d3f31809da4fd9e8c5eee8aa7276cb
joric10 <balconjoric_bscs@plmun.edu.ph>
2025-02-18 13:42:31 +0800

    Submitting OBJPROG Week 05 - Laboratory # 14

---
+++ MethodDemo.java
@@ -0,0 +1,21 @@
+public class MethodDemo {
+    public static void main(String[] args) {
+        displayWelcomeMessage();
+        
+        int value1 = 20;
+        int value2 = 30;
+        double result = calculateAverage(value1, value2);
+        
+        System.out.println("The average is: " + result);
+    }
+
+    // Method to display a welcome message
+    public static void displayWelcomeMessage() {
+        System.out.println("Welcome to our program!");
+    }
+
+    // Method to calculate the average of two numbers
+    public static double calculateAverage(int num1, int num2) {
+        return (num1 + num2) / 2.0;
+    }
+}
